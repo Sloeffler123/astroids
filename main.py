42b66aec55b8d3f4269370bf2ed7c909cee04b18
Sloeffler123 <samloeffler123@gmail.com>
2025-03-19 09:05:12 -0500

    player movement

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    player.update(dt)
     screen.fill('black')
     player.draw(screen)
+    
     pygame.display.flip()
     dt += clock.tick(60) / 1000
     
