50c47608e6f122ee7bb2bbeb4025034eb33b4ade
Sloeffler123 <samloeffler123@gmail.com>
2025-03-19 08:48:36 -0500

    Player on screen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 
 def main():
@@ -10,13 +10,18 @@ def main():
     print(f'Screen height: {SCREEN_HEIGHT}')
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
+clock = pygame.time.Clock()
+dt = 0
+player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill('black')
+    player.draw(screen)
     pygame.display.flip()
-
+    dt += clock.tick(60) / 1000
+    
 if __name__ == '__main__':
     main()
